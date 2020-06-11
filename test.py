0570c4c62aea74d45a3740a88ae76f21d8f34621
Devin Willis <gatordevin@gmail.com>
2020-06-11 16:13:08 +0000

    Fixed test.py

--- test.py
+++ test.py
@@ -1,33 +1,34 @@
 from EasyCamera.EasyCamera import Camera, Pipelines
-from AIManager.AIManager import AIManager, AIModels
+from AIManager.AIManager import AI, AIModels
 from time import sleep
-import os
 
-CSI = Camera(0)
-USB = Camera(1)
+CSI = Camera(device=0)
+USB = Camera(device=1)
+TPU = AI(tpu="/dev/apex_0")
 
-AIMan = AIManager()
+CSI_H264 = CSI.add(pipeline_type=Pipelines.H264, size=(640,480), frame_rate=30)
+USB_H264 = USB.add(pipeline_type=Pipelines.H264, size=(640,480), frame_rate=30)
 
-CSI.add(pipeline_type=Pipelines.H264, size=(640,480), frame_rate=30, tag="csi_h264")
-USB.add(pipeline_type=Pipelines.H264, size=(640,480), frame_rate=30, tag="usb_h264")
-
-CSI.add_AI(AI_class=AIMan, model_type=AIModels.detectFace, frame_rate=30, tag="csi_face")
-USB.add_AI(AI_class=AIMan, model_type=AIModels.detectFace, frame_rate=30, tag="usb_face")
+#CSI_DETECT = TPU.add(model_type=AIModels.detectFace, camera=CSI)
+# CSI.add_AI(AI_class=AIMan, model_type=AIModels.detectFace, frame_rate=30, tag="csi_face")
+# USB.add_AI(AI_class=AIMan, model_type=AIModels.detectFace, frame_rate=30, tag="usb_face")
 
 CSI.start()
-if os.path.exists(f'/dev/video{str(USB.device)}'):
-    USB.start()
+USB.start()
 
 while True:
-
-    #if(CSI.data("csi_face")):
-    #    print(CSI.get_image("csi_face"), "CSI Face Data")
+    if(CSI_H264):
+        print(bytes(CSI_H264))
+    # if(CSI_DETECT):
+    #     print(CSI_DETECT)
+    # if(CSI.data("csi_face")):
+    #     print(CSI.get_image("csi_face"), "CSI Face Data")
     
-    print(CSI.get_fps("csi_face"), "CSI Face")
+    # print(CSI.get_fps("csi_face"), "CSI Face")
 
-    if(AIMan.data("csi_face")):
-        print(AIMan.get_data("csi_face"), "CSI Face AI")
-    #if(AIMan.data("usb_face")):
-    #    print(AIMan.get_data("usb_face"), "USB Face AI")
+    # if(AIMan.data("csi_face")):
+    #     print(AIMan.get_data("csi_face"), "CSI Face AI")
+    # if(AIMan.data("usb_face")):
+    #     print(AIMan.get_data("usb_face"), "USB Face AI")
     
     sleep(0.0001)
