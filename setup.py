f49351e8de3fc6f9bce91dbb5b3a70fb92bfa083
James Warszycki <jwarszycki2017@fau.edu>
2020-06-14 22:04:47 +0000

    added MANIFEST

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="Easy_Coral",
-    version="0.0.1",
+    version="0.0.10",
     author="James Warszycki, Devin Willis",
     author_email="jwarszycki2017@fau.edu",
     description="Google Coral AI and Camera Manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     python_requires='>=3.6',
 )
